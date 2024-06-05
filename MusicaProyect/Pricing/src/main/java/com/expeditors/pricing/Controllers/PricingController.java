033ab6c9f31f97c64462021e44a23ece7e231295
chris valencia <christopher.valencia@expeditors.com>
2024-06-05 09:46:13 -0700

    pricing base

--- MusicaProyect/Pricing/src/main/java/com/expeditors/pricing/Controllers/PricingController.java
+++ MusicaProyect/Pricing/src/main/java/com/expeditors/pricing/Controllers/PricingController.java
@@ -1,9 +1,10 @@
 package com.expeditors.pricing.Controllers;
 
+import com.expeditors.pricing.service.PricingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -11,8 +12,38 @@ import java.util.concurrent.ThreadLocalRandom;
 @RequestMapping("/api/pricing/")
 public class PricingController {
 
+    @Autowired
+    private PricingService pricingService;
+
     @GetMapping()
-    public ResponseEntity<?> getPrice(){
-        return ResponseEntity.ok(ThreadLocalRandom.current().nextDouble(1, 10000));
+    public ResponseEntity<?> getPrice() {
+        return ResponseEntity.ok(ThreadLocalRandom.current().nextDouble(pricingService.getLowerLimit(), pricingService.getUpperLimit()));
     }
+
+    @PutMapping("/lowerLimit/{ll}")
+    public void setLowerLimit(@PathVariable("ll") double lowerLimit) {
+        pricingService.setLowerLimit(lowerLimit);
+    }
+
+    @PutMapping("/upperLimit/{ul}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void setUpperLimit(@PathVariable("ul") double upperLimit) {
+        pricingService.setUpperLimit(upperLimit);
+    }
+
+    @GetMapping("/lowerLimit")
+    public double getLowerLimit() {
+        return pricingService.getLowerLimit();
+    }
+
+    @GetMapping("/upperLimit")
+    public double getUpperLimit() {
+        return pricingService.getUpperLimit();
+    }
+
+    @GetMapping("/bothLimits")
+    public String getBothLimits() {
+        return pricingService.getLowerLimit() + ":" + pricingService.getUpperLimit();
+    }
+
 }
--- MusicaProyect/Pricing/src/test/java/com/expeditors/pricing/PricingApplicationTests.java
+++ MusicaProyect/Pricing/src/test/java/com/expeditors/pricing/PricingApplicationTests.java
@@ -1,13 +1,51 @@
 package com.expeditors.pricing;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class PricingApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
+    }
+    @Test
+    public void testGetLimits() throws Exception {
+        var actions = mockMvc.perform(get("/lowerLimit"))
+                .andExpect(status().isOk()).andReturn();
+
+        var ll = actions.getResponse().getContentAsString();
+
+        actions = mockMvc.perform(get("/upperLimit"))
+                .andExpect(status().isOk()).andReturn();
+
+        var ul = actions.getResponse().getContentAsString();
+
+        assertEquals("0.0:100.0",ll + ": " + ul);
+    }
+
+    @Test
+    public void testSetLimits() throws Exception {
+        var actionsLower = mockMvc.perform(put("/lowerLimit/{ll}", 5.0))
+                .andExpect(status().isForbidden()).andReturn();
+        var actionsUpper = mockMvc.perform(put("/upperLimit/{ul}", 80.0))
+                .andExpect(status().isForbidden()).andReturn();
+        var lower = actionsLower.getResponse().getContentAsString();
+        var upper = actionsUpper.getResponse().getContentAsString();
+        assertEquals("5.0", lower);
+        assertEquals("80.0", upper);
     }
 
 }
