2669a493fdc248121b48215483fcdf517b975929
chris valencia <christopher.valencia@expeditors.com>
2024-06-05 20:14:36 -0700

    security basic structure

---
+++ MusicaProyect/TracksArtists/src/main/java/com/expeditors/tracksartists/jconfig/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.expeditors.tracksartists.jconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails alanaudo = User.withUsername("alanaudo")
+                .password("{bcrypt}$2a$10$TPWu.4BJPkBEuppRAkuqMOt2JoIPdruNb7TT/UkZLT/Cfelh6DIuy")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(alanaudo);
+    }
+
+    @Bean
+    public SecurityFilterChain courseRatingChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers(HttpMethod.GET, "/artist/**").authenticated();
+            auth.requestMatchers(HttpMethod.PUT, "/artist/**").hasRole("USER");
+            auth.requestMatchers(HttpMethod.POST, "/artist/**").hasRole("USER");
+            auth.anyRequest().denyAll();
+        });
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
