c37ca755ee5c3c129c55f4b2347ec2285ae2f8f1
chris valencia <christopher.valencia@expeditors.com>
2024-06-05 20:25:47 -0700

    security changes

--- MusicaProyect/Pricing/src/main/java/com/expeditors/pricing/jconfig/SecurityConfig.java
+++ MusicaProyect/Pricing/src/main/java/com/expeditors/pricing/jconfig/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
--- MusicaProyect/TracksArtists/src/main/java/com/expeditors/tracksartists/jconfig/SecurityConfig.java
+++ MusicaProyect/TracksArtists/src/main/java/com/expeditors/tracksartists/jconfig/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.expeditors.tracksartists.jconfig;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
